222b5856562ada6d99be7e90e958f3795985299c
STester04 <STester04@fasbaldoyle.ie>
2017-08-31 12:51:50 +0100

    setup and index page

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Eric's Chat room</title>
+
+    <link rel="stylesheet" href="css/stylesheet.css">
+
+  </head>
+  <body>
+    <div id="container">
+
+
+
+    <div id="chat_box">
+
+      <div id="chat_data">
+
+        <span>Eric:</span>
+        <span> Hey!</span>
+        <span> 12.44</span>
+
+      </div>
+
+    </div>
+
+    <form action="index.html" method="post">
+
+      <input type="text" placeholder="enter name here" name="name" required>
+        <textarea type="text" placeholder="enter message here" name="message"  required></textarea>
+          <input type="submit" name="submit" value="send it">
+
+    </form>
+
+    </div>
+
+  </body>
+</html>
